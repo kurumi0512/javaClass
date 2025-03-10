5d328dd2af6ba3deb7837922a438e9132a5b1cb1
kurumi0512 <foreverlove0512t@gmail.com>
2025-03-10 14:20:03 +0800

    250310上課內容

---
+++ Test2.java
@@ -0,0 +1,5 @@
+public class Test2 {
+  public static void main(String[] args) {
+    System.out.println("檔案名字有錯誤");
+  }
+}
---
+++ Test4.java
@@ -0,0 +1,5 @@
+public class Test4 {
+  public static void main(String[] xyz) {
+    System.out.println("少了static");
+  }
+}
